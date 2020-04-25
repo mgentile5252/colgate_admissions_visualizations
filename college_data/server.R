4e2fdf10f74e8edff92c35158495df4f60c45fd7
mgentile5252 <mgentile52@gmail.com>
2020-04-24 22:36:21 -0400

    Changed UI around. Created textInput to add non patriot league schools into the comparison

--- college_data/server.R
+++ college_data/server.R
@@ -14,109 +14,360 @@ library(ggalt)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
       
-      observeEvent(input$create_plot,{
-            y <- input$metric
-            y <- as.numeric(y)
+      
+      observeEvent(input$make_list,{
+            current_schools <- input$colleges
             
-            url_p1 <- "https://www.collegedata.com/college/"
-            url_p2 <- "/?tab=profile-admission-tab"
+           
             
-            school_list <- input$colleges
+           # observeEvent(input$add_school, {
+            #      new_school <- input$text_college
+             #     current_schools <- c(current_schools, new_school)
+             #})
             
-            url_list <- make_urls(url_p1,school_list,url_p2)
-            num_schools <- length(school_list)
-            #print(num_schools) -- good
+            new_school <- input$text_college
+            current_schools <- c(current_schools, new_school)
             
-            info_df_list <- list()
-            for (i in 1:num_schools){
-                  #print(i)
-                  school_tibble <- build_info_char(url_list[[i]]) %>%
-                        build_info_df(school_list[[i]]) 
-
-                  info_df_list[[i]] <- school_tibble
+            print(current_schools)
+            
+            
+            current_schools <- unlist(strsplit(current_schools, ", "))
+            
+            print(current_schools)
+            
+            # observeEvent(input$finalize,{
+            #       output$school_list <- renderText({
+            #             print(current_schools)})
+            #       
+            # })
+            
+            
+            
+            
+            observeEvent(input$create_plot, {
+                  y <- input$metric
+                  y <- as.numeric(y)
                   
-                 # print(info_df_list)
-                         }
-            
-            #if (y in c("SAT Math (middle 50% range)", "SAT EBRW (middle 50% range)","ACT (middle 50% range)")){
-            # }
-            # else{
-            # }
-            print(y)
-            `%notin%` <- Negate(`%in%`)
-            
-            if (y %notin% c(12,13,15,16,18,19)){
+                  ############
+                  print(current_schools)
                   
+                  url_p1 <- "https://www.collegedata.com/college/"
+                  url_p2 <- "/?tab=profile-admission-tab"
                   
-                  plot_vals <- c()
+                  school_list <- current_schools   
+                  
+                  url_list <- make_urls(url_p1,school_list,url_p2)
+                  num_schools <- length(school_list)
+                  
+                  info_df_list <- list()
                   for (i in 1:num_schools){
+                        print(i)
+                        school_tibble <- build_info_char(url_list[[i]]) %>%
+                              build_info_df(school_list[[i]]) 
                         
-                        plot_vals[i] <- info_df_list[[i]][[y]]
-                        #print(plot_vals)
+                        info_df_list[[i]] <- school_tibble
+                        
+                        
                   }
                   
+                  `%notin%` <- Negate(`%in%`)
                   
-                  
-                  plot_df <- tibble(school = school_list, val = plot_vals)
-      
-                  output$mainPlot <- renderPlot({
+                  if (y %notin% c(12,13,15,16,18,19)){
                         
+                        
+                        plot_vals <- c()
+                        for (i in 1:num_schools){
+                              plot_vals[i] <- info_df_list[[i]][[y]]
+                        }
+                        
+                        
+                        
+                        plot_df <- tibble(school = school_list, val = plot_vals)
+                        
+                        
+                        output$mainPlot <- renderPlot({
+                              
                               ggplot(data=plot_df, aes(x=school, y=val)) +
                                     geom_bar(stat="identity", fill="steelblue")+
                                     geom_text(aes(label=val), vjust=1.6, color="white", size=3.5)+
                                     theme_minimal()+
                                     ggtitle("College Comparison")+
+                                    theme(plot.title = element_text(size = 30, face = "bold"))+
                                     xlab("School Name") +
                                     ylab("Comparison Metric Value")
                               
-                  })
-            }
-                  
-            else{
-                  plot_valsH <- c()
-                  plot_valsL <- c()
-                  for (i in 1:num_schools){
-                        
-                        #print(info_df_list[[i]])
-                        
-                        #print(info_df_list[[i]][[y]])
-                        #print(info_df_list[[i]][[y+1]])
-                        
-                        plot_valsH[i] <- info_df_list[[i]][[y]]
-                        plot_valsL[i] <- info_df_list[[i]][[y+1]]
+                        })
                   }
                   
-                  
-                  
-                  plot_df <- tibble(school = school_list, valH = plot_valsH, valL = plot_valsL)
-                  #print(plot_df)
+                  else{
+                        plot_valsH <- c()
+                        plot_valsL <- c()
                         
-                  output$mainPlot <- renderPlot({     
-                        ggplot(data = plot_df, aes(y = school, x = valH, xend = valL, size = 3.5)) +
-                              geom_dumbbell(color="steel blue", 
-                                            size_x=3.5, 
-                                            size_xend = 3.5)+
-                              geom_text(color="black", size=6,
-                                        aes(x=valL, label=valL))+
-                              geom_text(aes(x=valH, label=valH), 
-                                        color="black", size=6)+
-                              coord_flip() +
-                              theme_minimal()+
-                              ggtitle("College Comparison") +
-                              ylab("School Name") +
-                              xlab("Comparison Metric Value")
+                        for (i in 1:num_schools){
+                              
+                              
+                              
+                              plot_valsH[i] <- info_df_list[[i]][[y]]
+                              plot_valsL[i] <- info_df_list[[i]][[y+1]]
+                        }
+                        
+                        
+                        plot_df <- tibble(school = school_list, valH = plot_valsH, valL = plot_valsL)
+                        
+                        output$mainPlot <- renderPlot({     
+                              ggplot(data = plot_df, aes(y = school, x = valH, xend = valL, size = 5)) +
+                                    geom_dumbbell(color="steel blue", 
+                                                  size_x=10, 
+                                                  size_xend = 10)+
+                                    geom_text(color="white", size=4.5,
+                                              aes(x=valL, label=valL))+
+                                    geom_text(aes(x=valH, label=valH), 
+                                              color="white", size=4.5)+
+                                    coord_flip() +
+                                    theme_minimal()+
+                                    theme(plot.title = element_text(size = 30, face = "bold"))+
+                                    ggtitle("College Comparison") +
+                                    ylab("School Name") +
+                                    xlab("Comparison Metric Value")
                         })
                         
                   }
-                 
+                  
+                  
             })
             
             
- 
-             
-       # })
+      })
+      
+})
+#       observeEvent(input$make_list,{
+#             current_schools <- input$colleges
+#             #print(current_schools)
+#       })
+#       
+#       
+#       observeEvent(input$add_school, {
+#             
+#             new_school <- input$text_college
+#             
+#             current_schools <- c(current_schools, new_school)
+#             
+#             
+#       })
+#       
+#       
+#       observeEvent(input$finalize,{
+#             final_school_table <- as.table(current_schools)
+#             output$school_table <- renderTable(final_school_table)
+#             
+#       })
+#       
+#      
+#       
+#       
+#       
+#       observeEvent(input$create_plot, {
+#             
+#             y <- input$metric
+#             y <- as.numeric(y)
+#              
+#             url_p1 <- "https://www.collegedata.com/college/"
+#             url_p2 <- "/?tab=profile-admission-tab"
+#             
+#             school_list <- current_schools   
+#             
+#             url_list <- make_urls(url_p1,school_list,url_p2)
+#             num_schools <- length(school_list)
+#             
+#             info_df_list <- list()
+#             for (i in 1:num_schools){
+#                   print(i)
+#                   school_tibble <- build_info_char(url_list[[i]]) %>%
+#                   build_info_df(school_list[[i]]) 
+#             
+#                   info_df_list[[i]] <- school_tibble
+#             
+#             
+#                   }
+#             
+#             `%notin%` <- Negate(`%in%`)
+#                   
+#             if (y %notin% c(12,13,15,16,18,19)){
+#                         
+#                          
+#                   plot_vals <- c()
+#                   for (i in 1:num_schools){
+#                                plot_vals[i] <- info_df_list[[i]][[y]]
+#                          }
+#                          
+#                          
+#                          
+#                   plot_df <- tibble(school = school_list, val = plot_vals)
+#                   
+#                   
+#                   output$mainPlot <- renderPlot({
+#                                            
+#                               ggplot(data=plot_df, aes(x=school, y=val)) +
+#                                     geom_bar(stat="identity", fill="steelblue")+
+#                                     geom_text(aes(label=val), vjust=1.6, color="white", size=3.5)+
+#                                     theme_minimal()+
+#                                     ggtitle("College Comparison")+
+#                                     xlab("School Name") +
+#                                     ylab("Comparison Metric Value")
+#                                                 
+#                                      })
+#             }
+#             
+#             else{
+#                   plot_valsH <- c()
+#                   plot_valsL <- c()
+#                   
+#                   for (i in 1:num_schools){
+#                                     
+#                   
+#                    
+#                         plot_valsH[i] <- info_df_list[[i]][[y]]
+#                         plot_valsL[i] <- info_df_list[[i]][[y+1]]
+#                   }
+#                   
+#                                
+#                              
+#                             
+#                   plot_df <- tibble(school = school_list, valH = plot_valsH, valL = plot_valsL)
+#                  
+#                                      
+#                   output$mainPlot <- renderPlot({     
+#                               ggplot(data = plot_df, aes(y = school, x = valH, xend = valL, size = 3.5)) +
+#                                     geom_dumbbell(color="steel blue", 
+#                                                 size_x=3.5, 
+#                                                 size_xend = 3.5)+
+#                                     geom_text(color="black", size=6,
+#                                                 aes(x=valL, label=valL))+
+#                                     geom_text(aes(x=valH, label=valH), 
+#                                                 color="black", size=6)+
+#                                     coord_flip() +
+#                                     theme_minimal()+
+#                                     ggtitle("College Comparison") +
+#                                            ylab("School Name") +
+#                                            xlab("Comparison Metric Value")
+#                         })
+#                                     
+#                   }
+#             
+#             
+#             
+#             
+#       })
+# })
+#       
+      
+      
+      # observeEvent(input$create_plot,{
+      #       y <- input$metric
+      #       y <- as.numeric(y)
+      #       
+      #       url_p1 <- "https://www.collegedata.com/college/"
+      #       url_p2 <- "/?tab=profile-admission-tab"
+      #       
+      #       school_list <- input$colleges
+      #       
+      #       observeEvent(input$add_school, {
+      #             new_school <- input$text_college
+      #             #print(class(new_school))
+      #             #print(class(school_list))
+      #             school_list <- c(school_list, new_school)
+      #             print(school_list)
+      #       })
+      #       print(class(school_list))
+      #       print(school_list)
+      #       url_list <- make_urls(url_p1,school_list,url_p2)
+      #       num_schools <- length(school_list)
+      #       
+      #       print(num_schools)
+      #       #print(school_list)
+      #       #print(num_schools) -- good
+      #       
+      #       info_df_list <- list()
+      #       for (i in 1:num_schools){
+      #             print(i)
+      #             school_tibble <- build_info_char(url_list[[i]]) %>%
+      #                   build_info_df(school_list[[i]]) 
+      # 
+      #             info_df_list[[i]] <- school_tibble
+      #             
+      #             print(info_df_list)
+      #                    }
+      #       
+      #     
+      #       print(y)
+      #       `%notin%` <- Negate(`%in%`)
+      #       
+      #       if (y %notin% c(12,13,15,16,18,19)){
+      #             
+      #             
+      #             plot_vals <- c()
+      #             for (i in 1:num_schools){
+      #                   
+      #                   plot_vals[i] <- info_df_list[[i]][[y]]
+      #                   #print(plot_vals)
+      #             }
+      #             
+      #             
+      #             
+      #             plot_df <- tibble(school = school_list, val = plot_vals)
+      # 
+      #             output$mainPlot <- renderPlot({
+      #                   
+      #                         ggplot(data=plot_df, aes(x=school, y=val)) +
+      #                               geom_bar(stat="identity", fill="steelblue")+
+      #                               geom_text(aes(label=val), vjust=1.6, color="white", size=3.5)+
+      #                               theme_minimal()+
+      #                               ggtitle("College Comparison")+
+      #                               xlab("School Name") +
+      #                               ylab("Comparison Metric Value")
+      #                         
+      #             })
+      #       }
+      #             
+      #       else{
+      #             plot_valsH <- c()
+      #             plot_valsL <- c()
+      #             for (i in 1:num_schools){
+      #                   
+      #                   #print(info_df_list[[i]])
+      #                   
+      #                   #print(info_df_list[[i]][[y]])
+      #                   #print(info_df_list[[i]][[y+1]])
+      #                   
+      #                   plot_valsH[i] <- info_df_list[[i]][[y]]
+      #                   plot_valsL[i] <- info_df_list[[i]][[y+1]]
+      #             }
+      #             
+      #             
+      #             
+      #             plot_df <- tibble(school = school_list, valH = plot_valsH, valL = plot_valsL)
+      #             #print(plot_df)
+      #                   
+      #             output$mainPlot <- renderPlot({     
+      #                   ggplot(data = plot_df, aes(y = school, x = valH, xend = valL, size = 3.5)) +
+      #                         geom_dumbbell(color="steel blue", 
+      #                                       size_x=3.5, 
+      #                                       size_xend = 3.5)+
+      #                         geom_text(color="black", size=6,
+      #                                   aes(x=valL, label=valL))+
+      #                         geom_text(aes(x=valH, label=valH), 
+      #                                   color="black", size=6)+
+      #                         coord_flip() +
+      #                         theme_minimal()+
+      #                         ggtitle("College Comparison") +
+      #                         ylab("School Name") +
+      #                         xlab("Comparison Metric Value")
+      #                   })
+      #                   
+      #             }
+      #            
+      #       })
+            
        
   
-})
--- college_data/ui.R
+++ college_data/ui.R
@@ -14,58 +14,127 @@ library(rvest)
 library(stringr)
 library(tidyverse)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Patriot League Admission Data Visualizations"),
+  title = "College Admission Data Visualizations",
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-          checkboxGroupInput("colleges",
-                   "Select Colleges to Compare:",
-                   c(
-                   "American University" = "American University",
-                   "Boston University" = "Bo,ton University",
-                   "Bucknell University" = "Bucknell University",
-                   "Colgate University" = "Colgate University",
-                   "College of the Holy Cross" =  "College of the Holy Cross",
-                   "Lafayette College" = "Lafayette College",
-                   "Lehigh University" =  "Lehigh University",
-                   "Loyola University Maryland" = "Loyola University Maryland"
-                   )
-                   ),
-            selectInput("metric",
-                        "Select Admission Metric to Compare:",
-                              c(
-                              "Total Applicants" = 2,
-                              "Total Admitted" = 3,
-                              "Acceptance Rate" = 4,
-                              "Acceptance Rate - Male" = 5,
-                              "Acceptance Rate - Female" = 6,    
-                              "Total Enrolled" = 7,
-                              "% of Admitted that Enrolled" = 8,
-                              "Early Decision Accepatance Rate" = 9,
-                              "Early Decision Applicants" = 10,
-                              "Average High School GPA" =11,
-                              "SAT Math (middle 50% range)" = 12,
-                              "SAT EBRW Average" = 14,
-                              "SAT EBRW (middle 50% range)" = 15,
-                              "ACT Average" = 17,
-                              "ACT (middle 50% range)" = 18
-                           
-                              )
-                        ),
-          actionButton("create_plot", "Compare!")
-          
-                  
-       )
-    ,
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("mainPlot")
-    )
+  
+  plotOutput("mainPlot"),
+  
+  hr(),
+  hr(), 
+  
+  fluidRow(
+        column(3,
+               h4("Select Comparison Metric"),
+               selectInput("metric",
+                           "Select Admission Metric to Compare:",
+                           c(
+                                 "Total Applicants" = 2,
+                                 "Total Admitted" = 3,
+                                 "Acceptance Rate" = 4,
+                                 "Acceptance Rate - Male" = 5,
+                                 "Acceptance Rate - Female" = 6,    
+                                 "Total Enrolled" = 7,
+                                 "% of Admitted that Enrolled" = 8,
+                                 "Early Decision Accepatance Rate" = 9,
+                                 "Early Decision Applicants" = 10,
+                                 "Average High School GPA" =11,
+                                 "SAT Math (middle 50% range)" = 12,
+                                 "SAT EBRW Average" = 14,
+                                 "SAT EBRW (middle 50% range)" = 15,
+                                 "ACT Average" = 17,
+                                 "ACT (middle 50% range)" = 18
+                                 
+                           )
+               ),
+               actionButton("create_plot", "Compare!", style='padding:12.5px; font-size:150%')
+        ),
+        
+        column(4, offset = 1,
+               checkboxGroupInput("colleges",
+                                  "Select Patriot League Schools to Compare:",
+                                  c(
+                                        "American University" = "American University",
+                                        "Boston University" = "Bo,ton University",
+                                        "Bucknell University" = "Bucknell University",
+                                        "Colgate University" = "Colgate University",
+                                        "College of the Holy Cross" =  "College of the Holy Cross",
+                                        "Lafayette College" = "Lafayette College",
+                                        "Lehigh University" =  "Lehigh University",
+                                        "Loyola University Maryland" = "Loyola University Maryland"
+                                  )
+               )
+               
+               #actionButton("add_school", "Add School to Selection!")
+              
+        ),
+        column(4,
+               textInput("text_college", "Add School Not Listed (separated by comma):"),
+               actionButton("make_list", "Update Selected Schools!",style='padding:22.5px; font-size:150%')
+               
+        )
   )
-))
+)
+)
+       
+        
+        
+        
+        
+        
+        
+                
+  
+#   # Sidebar with a slider input for number of bins 
+#   sidebarLayout(
+#     sidebarPanel(
+#           checkboxGroupInput("colleges",
+#                    "Select Colleges to Compare:",
+#                    c(
+#                    "American University" = "American University",
+#                    "Boston University" = "Bo,ton University",
+#                    "Bucknell University" = "Bucknell University",
+#                    "Colgate University" = "Colgate University",
+#                    "College of the Holy Cross" =  "College of the Holy Cross",
+#                    "Lafayette College" = "Lafayette College",
+#                    "Lehigh University" =  "Lehigh University",
+#                    "Loyola University Maryland" = "Loyola University Maryland"
+#                    )
+#                    ),
+#             textInput("text_college", "Add School Not Listed:", "(e.g. Yale University or Williams College)"),
+#             actionButton("add_school", "Add School to Selection!"),
+#             selectInput("metric",
+#                         "Select Admission Metric to Compare:",
+#                               c(
+#                               "Total Applicants" = 2,
+#                               "Total Admitted" = 3,
+#                               "Acceptance Rate" = 4,
+#                               "Acceptance Rate - Male" = 5,
+#                               "Acceptance Rate - Female" = 6,    
+#                               "Total Enrolled" = 7,
+#                               "% of Admitted that Enrolled" = 8,
+#                               "Early Decision Accepatance Rate" = 9,
+#                               "Early Decision Applicants" = 10,
+#                               "Average High School GPA" =11,
+#                               "SAT Math (middle 50% range)" = 12,
+#                               "SAT EBRW Average" = 14,
+#                               "SAT EBRW (middle 50% range)" = 15,
+#                               "ACT Average" = 17,
+#                               "ACT (middle 50% range)" = 18
+#                            
+#                               )
+#                         ),
+#           actionButton("create_plot", "Compare!")
+#           
+#                   
+#        )
+#     ,
+#     
+#     # Show a plot of the generated distribution
+#     mainPanel(
+#        plotOutput("mainPlot")
+#     )
+#   )
+# ))
